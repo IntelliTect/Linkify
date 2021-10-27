5790a98da9748544ec2a0aebcd41e57dfd67d054
Joseph Riddle <joeriddles10@gmail.com>
2021-10-27 10:58:39 -0700

    Add file logging

--- linkify/linkify.py
+++ linkify/linkify.py
@@ -9,8 +9,20 @@ from .db_context import DbContext
 from .posts import Post
 
 
-root_logger = logging.getLogger()
-root_logger.setLevel(logging.INFO)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
+
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setLevel(logging.DEBUG)
+stdout_handler.setFormatter(formatter)
+
+file_handler = logging.FileHandler('logs/log.txt')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stdout_handler)
 
 
 INTELLITECT_LINK_PATTERN = re.compile(r"http[s]?:\/\/intellitect\.com")
